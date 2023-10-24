ee2b7c7e1e3a2197859a336221061ef807ab8d21
fjfj10 <kimmisuk2218@gmail.com>
2023-10-24 16:39:51 +0900

    feat: DB에서 카테고리 정보 가져오기 완료

--- src/main/java/com/korit/board/controller/BoardController.java
+++ src/main/java/com/korit/board/controller/BoardController.java
@@ -1,12 +1,9 @@
 package com.korit.board.controller;
 
-import com.korit.board.aop.annotation.ValidAop;
-import com.korit.board.dto.RegisterBoardReqDto;
-import com.korit.board.exception.ValidException;
+import com.korit.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -14,13 +11,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class BoardController {
 
-    @ValidAop
-    @PostMapping("/board/{category}")
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterBoardReqDto registerBoardReqDto, BindingResult bindingResult) {
+    private final BoardService boardService;
 
+    @GetMapping("/board/categories")
+    public ResponseEntity<?> getCategories() {
 
-        return ResponseEntity.ok(true);
+        return ResponseEntity.ok(boardService.getBoardCategoriesAll());
     }
 }
